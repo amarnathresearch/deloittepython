b05eb3013fd899038bf1743b4e890a628d6ce36e
Amarnath R <amarnathresearch@gmail.com>
2022-01-28 10:50:47 +0530

    Testing is added

--- sample8.py
+++ sample8.py
@@ -15,3 +15,32 @@ b[1] = 50
 
 print(id(a))
 print(id(b))
+
+a = {"next":{"next":{"next":{"next":{"next":{"next":{"final":{"employer":"Deloitte"}}}}}}}}
+d = a
+for item in d:
+    key = item
+    break
+while d[key]:
+    d = d[key]
+    print(d)
+    for item in d:
+        key = item
+        break
+    try:
+        d[key]
+    except:
+        break
+
+
+a = {"next":{"next":{"next":{"next":{"next":{"next":{"final":{"employer":"Deloitte"}}}}}}}}
+b = a["next"]
+while type(b)==type(a):
+    for i in b:
+        x=i
+    b=b[x]
+print(b)
+
+
+
+# Testing
